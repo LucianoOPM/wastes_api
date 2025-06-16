b525dfc8577c339e24b982187659a88c50709e28
Luciano Perez <jesus-61914@hotmail.com>
2025-06-16 08:01:07 -0600

    UPDATE Refactor for user filter schema

--- src/users/user.schema.ts
+++ src/users/user.schema.ts
@@ -1,8 +1,8 @@
 import { z } from 'zod';
+import { BaseQuerySchemaBuilder } from '@common/schemas/base-query.schema';
 
 const passwordRegex = new RegExp(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/, 'g');
 const orderList = ['email', 'firstName', 'lastName', 'idUser'] as const;
-const validOrder = ['asc', 'desc'] as const;
 
 export const CreateUserSchema = z
   .object({
@@ -35,9 +35,14 @@ export type UpdateUserDto = z.infer<typeof UpdateUserSchema>;
 export const UpdateStatusSchema = CreateUserSchema.pick({ isActive: true });
 export type UpdateStatusDto = z.infer<typeof UpdateStatusSchema>;
 
-export const FilterUserSchema = z.object({
-  orderBy: z.enum(orderList, { message: `Just ordereable by ${orderList.join(', ')}` }).default('idUser'),
-  order: z.enum(validOrder, { message: `Just order shoul be $${validOrder.join(', ')}` }).default('asc'),
+export const FilterUserSchema = BaseQuerySchemaBuilder.buildFilterSchema(orderList, {
+  profileId: z.coerce
+    .number({ invalid_type_error: 'Profile ID should be a number' })
+    .int({ message: 'Profile ID should be an integer' })
+    .optional(),
+  email: z.string({ message: 'Email should be a string' }).email({ message: 'Email should be a valid format' }).optional(),
+  firstName: z.string({ message: 'Fist name should be a valid string' }).optional(),
+  lastName: z.string({ message: 'Fist name should be a valid string' }).optional(),
   isActive: z.preprocess(
     (val) => {
       if (val === '1' || val === 1 || val === true) return true;
@@ -46,24 +51,6 @@ export const FilterUserSchema = z.object({
     },
     z.boolean({ invalid_type_error: 'Is Active should be a valid boolean' }).optional(),
   ),
-  limit: z.coerce
-    .number({ message: 'Limit should be a valid number' })
-    .int({ message: 'Limit should be a valid integer' })
-    .min(1, { message: 'Limit should be at least 1' })
-    .max(100, { message: 'Limit cannot exceed 100' })
-    .default(10),
-  page: z.coerce
-    .number({ message: 'Page should be a valid number' })
-    .int({ message: 'Page should be a valid integer' })
-    .min(1, { message: 'Page should be at least 1' })
-    .default(1),
-  profileId: z.coerce
-    .number({ invalid_type_error: 'Profile ID should be a number' })
-    .int({ message: 'Profile ID should be an integer' })
-    .optional(),
-  email: z.string({ message: 'Email should be a string' }).email({ message: 'Email should be a valid format' }).optional(),
-  firstName: z.string({ message: 'Fist name should be a valid string' }).optional(),
-  lastName: z.string({ message: 'Fist name should be a valid string' }).optional(),
 });
 
 export type FilterUserDto = z.infer<typeof FilterUserSchema>;
